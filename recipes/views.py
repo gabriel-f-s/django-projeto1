8c4bf7104c9475c68fc7ab93f877e78445c01435
Gabriel Fonseca Silva <gabrielmiguel1309@gmail.com>
2024-10-16 06:49:19 -0300

    Configuração de settings.py e a vinculação do app com templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,10 @@
 
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("home")
+    return render(request, 'recipes/home.html')
 
 
 def sobre(request):
